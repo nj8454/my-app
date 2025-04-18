592f9b33e86b5d9272fd3f58bd9a2ba0b5dc9947
Nikhil Jain <nj8454@gmail.com>
2025-04-18 21:47:41 +0530

    commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,16 +28,16 @@ export default function TextForm(props) {
         <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value= {text} onChange={handleOnchange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value= {text} onChange={handleOnchange} style={{backgroundColobar: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'black':'white'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy to Clipboard</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy to Clipboard</button>
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your Text Summary:</h2>
-            <p> {text.split(" ").length}words and {text.length}characters</p>
-            <p> {.008*text.split(" ").length} Minutes read</p>
+            <p> {text.split(" ").filter((element)=>{return element.length!==0}).length}words and {text.length}characters</p>
+            <p> {.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h3>Preview</h3>
             <p>{text.length>0?text:"Enter something above to preview here"}</p>
         </div>
